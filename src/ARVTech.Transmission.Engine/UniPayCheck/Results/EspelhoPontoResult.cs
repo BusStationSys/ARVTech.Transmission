0a07e590dabd66732ccc920ddae332c794033113
ARVTech <busstationsys@gmail.com>
2023-06-20 00:06:42 -0300

    Inclusão da classe de serialização do Espelho de Ponto.

--- src/ARVTech.Transmission.Engine/UniPayCheck/Results/EspelhoPontoResult.cs
+++ src/ARVTech.Transmission.Engine/UniPayCheck/Results/EspelhoPontoResult.cs
@@ -4,6 +4,24 @@
 
     public class EspelhoPontoResult
     {
+        public string RazaoSocial { get; set; }
 
+        public string Cnpj { get; set; }
+
+        public string Competencia { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string DataAdmissao { get; set; }
+
+        public string Matricula { get; set; }
+
+        public string Nome { get; set; }
+
+        public string NumeroCtps { get; set; }
+
+        public string SerieCtps { get; set; }
+
+        public string UfCtps { get; set; }
     }
 }
